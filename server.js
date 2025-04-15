44d2e613ead698e1812b3bf0133a7431cf62a226
Sathvika B <serein.reverie.meraki@gmail.com>
2025-04-15 09:40:39 +0530

    Backend Continuous Assessment 2

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let user = null;
+
+app.post('/signup', (req, res) => {
+    const {username, email, password, dob} = req.body;
+    if(!username || !email || !password || !dob){
+        return res.status(400).send("All fields are required.");
+    }
+
+    if(password.length < 8 || password.length > 16){
+        return res.status(400).send("password must be 8 to 16 characters.");
+    }
+
+    user = {username, email, password, dob};
+    res.status(201).send("User created.");
+});
+
+app.get('/user', (req, res) => {
+    if(!user){ 
+        return res.status(404).send("User not found.");
+    }
+    res.json;
+});
+
+// PORT = 3000;
+app.listen(3000, () => console.log("Server is running on 3000."));
